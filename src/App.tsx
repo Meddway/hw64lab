c955af5fb22b70f308c5e8883f9953f86200dde5
Oleg Medvedev <meddway@gmail.com>
2023-11-30 20:35:21 +0600

    Создан первоначальный вид страницы: Установлены: bootstrap, react-router-dom, axios Добавлены компоненты по заданию Home, Add, About, Contacts, Toolbar Прописаны пути

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import Toolbar from './components/Toolbar/Toolbar';
+import About from './containers/About/About';
+import Contacts from './containers/Contacts/Contacts';
+import {Route, Routes} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Add from './containers/Add/Add';
+
 const App = () => (
   <>
-
+    <header>
+      <Toolbar/>
+    </header>
+    <main className="container-fluid">
+      <Routes>
+        <Route path="/" element={(<Home/>)}/>
+        <Route path="/posts" element={(<Home/>)}/>
+        <Route path="/new-post" element={(<Add/>)}/>
+        <Route path="/about" element={(<About/>)}/>
+        <Route path="/contacts" element={(<Contacts/>)}/>
+        <Route path="*" element={(<h1>Not Found!</h1>)}/>
+      </Routes>
+    </main>
   </>
 );
 
