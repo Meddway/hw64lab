d4b3e2e2928fc667ff35728c5e27f6a0ff493b6d
Oleg Medvedev <meddway@gmail.com>
2023-12-01 19:11:23 +0600

    Реализована часть задания по отображению отправленных постов на API, в компоненте Home: Для этого в компоненте Home был использован useState, useEffect, fetch, а также создана функция для ограничения символов в посте, в компоненте Home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,8 +1,50 @@
+import React, { useState, useEffect, } from 'react';
+import axiosApi from '../../axiosApi';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+interface Post {
+  id: string;
+  title?: string;
+  time: number;
+  content: string;
+}
+
+const Home: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axiosApi.get('/posts.json');
+        if (response.data) {
+          const postsArray: Post[] = Object.keys(response.data).map(key => ({
+            id: key,
+            ...response.data[key],
+          }));
+          setPosts(postsArray);
+        }
+      } catch (error) {
+        console.error('Error fetch posts:', error);
+      }
+    };
+    void fetch();
+  }, []);
+
+  const textLengthForPost = (text: string, maxLength: number) => {
+    return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+  };
+
   return (
-    <div>
-
+    <div className="mt-3">
+      {posts.map(post => (
+        <div key={post.id} className="mb-4 border p-3" style={{maxWidth: '480px'}} >
+          <p className="text-secondary">Created on: {new Date(post.time).toLocaleString()}</p>
+          <p>{textLengthForPost(post.content, 20)}</p>
+          <Link to={`/posts/${post.id}`} className="btn btn-outline-primary">
+            Read more
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
